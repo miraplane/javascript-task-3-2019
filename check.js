c2eb18e303f45f91deb73df17f67371e4593e62e
Irina Mochalova <IrinaMochalova@gmail.com>
2019-11-19 22:26:34 +0500

    Добавлен первый вариант решения

--- check.js
+++ check.js
@@ -1,5 +1,243 @@
 'use strict';
 
+const forObjects = {
+    containsKeys: function (keys) {
+        let result = true;
+        for (let key of keys) {
+            if (!this.workingObject.hasOwnProperty(key)) {
+                result = false;
+            }
+        }
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    },
+    hasKeys: function (keys) {
+        let result = true;
+        for (let key in this.workingObject) {
+            if (this.workingObject.hasOwnProperty(key) && keys.indexOf(key) === -1) {
+                result = false;
+            }
+        }
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    },
+    containsValue: function (value) {
+        let result = false;
+        for (let key in this.workingObject) {
+            if (this.workingObject.hasOwnProperty(key) && this.workingObject[key] === value) {
+                result = true;
+            }
+        }
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    },
+    containsValues: function (values) {
+        let result = true;
+        for (let value of values) {
+            if (!this.containsValue(value)) {
+                result = false;
+            }
+        }
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    },
+    hasValues: function (values) {
+        let result = true;
+        for (let key in this.workingObject) {
+            if (this.workingObject.hasOwnProperty(key) &&
+                values.indexOf(this.workingObject[key]) === -1) {
+                result = false;
+            }
+        }
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    },
+    hasValueType: function (key, type) {
+        let value = this.workingObject[key];
+        let typeValue = (type(value)).valueOf();
+
+        let result = typeof value === typeof typeValue;
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    }
+};
+
+const forArray = {
+    hasLength: function (length) {
+        let count = 0;
+        for (let prop in this.workingObject) {
+            if (this.workingObject.hasOwnProperty(prop)) {
+                count += 1;
+            }
+        }
+
+        let result = count === length;
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    }
+};
+
+const forString = {
+    hasWordsCount: function (count) {
+        let words = this.workingObject.split(' ');
+
+        let result = words.length === count;
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    }
+};
+
+const forFunction = {
+    hasParamsCount: function (count) {
+        let result = this.workingObject.length === count;
+        if (this.hasOwnProperty('not')) {
+            return !result;
+        }
+
+        return result;
+    }
+};
+
+exports.wrap = function (variable) {
+    return {
+        object: variable,
+        isNull: function () {
+            return this.object === null;
+        },
+        containsKeys: function (keys) {
+            let result;
+            try {
+                result = variable.check.containsKeys(keys);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        hasKeys: function (keys) {
+            let result;
+            try {
+                result = variable.check.hasKeys(keys);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        containsValues: function (values) {
+            let result;
+            try {
+                result = variable.check.containsValues(values);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        hasValues: function (values) {
+            let result;
+            try {
+                result = variable.check.hasValues(values);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        hasValueType: function (key, type) {
+            let result;
+            try {
+                result = variable.check.hasValueType(key, type);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        hasLength: function (length) {
+            let result;
+            try {
+                result = variable.check.hasLength(length);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        hasParamsCount: function (count) {
+            let result;
+            try {
+                result = variable.check.hasParamsCount(count);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        },
+        hasWordsCount: function (count) {
+            let result;
+            try {
+                result = variable.check.hasWordsCount(count);
+            } catch (err) {
+                return false;
+            }
+
+            return result;
+        }
+    };
+};
+
 exports.init = function () {
-    return;
+    Object.defineProperty(Object.prototype, 'check', {
+        configurable: true,
+        get: function () {
+            let type = typeof this;
+            if (Array.isArray(this)) {
+                type = 'array';
+            }
+            switch (type) {
+                case 'object':
+                    return Object.assign({ workingObject: this }, forObjects);
+                case 'array':
+                    return Object.assign({ workingObject: this }, forObjects, forArray);
+                case 'string':
+                    return Object.assign({ workingObject: this }, forArray, forString);
+                case 'function':
+                    return Object.assign({ workingObject: this }, forFunction);
+                default:
+                    return this;
+            }
+        }
+    });
+
+    Object.defineProperty(Object.prototype, 'not', {
+        configurable: true,
+        get: function () {
+            return Object.assign({ not: true }, this);
+        }
+    });
 };
--- check.spec.js
+++ check.spec.js
@@ -7,32 +7,80 @@ const check = require('./check');
 
 check.init();
 
-describe('Check', function () {
+describe('CheckObject', function () {
     const person = { name: 'John', age: 20 };
-    const numbers = [1, 2, 3];
-    const func = function (a, b) {
-        return a + b;
-    };
-    const str = 'some string';
 
-    it('should check that target hasKeys', function () {
+    it('should check that target containsKeys for object', function () {
+        assert.ok(person.check.containsKeys(['name']));
+    });
+
+    it('should check that target hasKeys for object', function () {
         assert.ok(person.check.hasKeys(['name', 'age']));
     });
 
-    it('should check that target hasValueType', function () {
+    it('should check that target containsValues for object', function () {
+        assert.ok(person.check.containsValues(['John']));
+    });
+
+    it('should check that target hasValues for object', function () {
+        assert.ok(person.check.hasValues([20, 'John']));
+    });
+
+    it('should check that target hasValueType for object incorrect', function () {
+        assert.ok(!person.check.hasValueType('age', String));
+    });
+
+    it('should check that target hasValueType for object', function () {
         assert.ok(person.check.hasValueType('name', String));
+    });
+});
+
+describe('CheckArray', function () {
+    const numbers = [1, 2, 3];
+    let arr = [1, 2];
+    arr[5] = 6;
+
+    it('should check that target containsKeys for array', function () {
+        assert.ok(numbers.check.containsKeys(['0', '2']));
+    });
+
+    it('should check not contains keys for array', function () {
+        assert.ok(!arr.check.containsKeys(['0', '3']));
     });
 
     it('should check that target hasKeys', function () {
         assert.ok(numbers.check.hasKeys(['0', '1', '2']));
     });
 
-    it('should check that target hasLength', function () {
+    it('should check has only this keys', function () {
+        assert.ok(arr.check.hasKeys(['0', '1', '5']));
+    });
+
+    it('should check that target containsValues for array', function () {
+        assert.ok(numbers.check.containsValues([2, 1]));
+    });
+
+    it('should check that target hasValues for array', function () {
+        assert.ok(numbers.check.hasValues([2, 1, 3]));
+    });
+
+    it('should check that target hasLength for array', function () {
         assert.ok(numbers.check.hasLength(3));
     });
 
-    it('should check that target containsValues', function () {
-        assert.ok(numbers.check.containsValues([2, 1]));
+    it('should check has length array with undefined elements', function () {
+        assert.ok(arr.check.hasLength(3));
+    });
+});
+
+describe('CheckStringAndFunction', function () {
+    const func = function (a, b) {
+        return a + b;
+    };
+    const str = 'some string';
+
+    it('should check that target hasLength for string', function () {
+        assert.ok(str.check.hasLength(11));
     });
 
     it('should check that target hasParamsCount', function () {
@@ -41,5 +89,62 @@ describe('Check', function () {
 
     it('should check that target hasWordsCount', function () {
         assert.ok(str.check.hasWordsCount(2));
+    });
+});
+
+describe('Null', function () {
+    const wrappedNull = check.wrap(null);
+    const wrappedString = check.wrap('hello');
+    const wrappedArray = check.wrap([1, 2, 3]);
+
+    it('Null is null', function () {
+        assert.ok(wrappedNull.isNull());
+    });
+
+    it('Null don`t have length', function () {
+        assert.ok(!wrappedNull.hasLength(5));
+    });
+
+    it('String is not null', function () {
+        assert.ok(!wrappedString.isNull());
+    });
+
+    it('Sting have property length', function () {
+        assert.ok(wrappedString.hasLength(5));
+    });
+
+    it('String haven`t property hasParamsCount', function () {
+        assert.ok(!wrappedString.hasParamsCount(2));
+    });
+
+    it('String haven`t property containsKeys', function () {
+        assert.ok(!wrappedString.containsKeys(['length']));
+    });
+
+    it('Array have property containsValue', function () {
+        assert.ok(wrappedArray.containsValues([1, 3]));
+    });
+
+    it('Array have property hasLength', function () {
+        assert.ok(!wrappedArray.hasLength(2));
+    });
+
+    it('Array haven`t property hasWordsCount', function () {
+        assert.ok(!wrappedArray.hasWordsCount(3));
+    });
+});
+
+describe('Not', function () {
+    let me = {
+        name: 'Roman',
+        age: 26
+    };
+
+    it('k', function () {
+        assert.ok(!me.check.not.containsKeys(['age', 'name']));
+    });
+
+    it('', function () {
+        assert.ok(me.check.not.hasValueType('age', String));
     });
 });
